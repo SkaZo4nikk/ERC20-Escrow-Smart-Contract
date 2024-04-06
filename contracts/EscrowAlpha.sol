fc5b07ae304d6a2ec9e6b7beb5d7787ea266efed
Izlaster <izmaylovle0@gmail.com>
2024-04-06 18:10:53 +0300

    add_token_change_id

--- contracts/EscrowAlpha.sol
+++ contracts/EscrowAlpha.sol
@@ -9,37 +9,33 @@ contract EscrowContract is Ownable {
     struct Deal {
         address client; //client
         address implementer; //implementer
+        IERC20 token;
         uint256 amount;
         bool dealCompleted;
         bool fundsDeposited;
     }
 
-    uint256 public nextDealId;
     mapping(uint256 => Deal) public deals;
-    IERC20 public token;
 
-    constructor(IERC20 _token) Ownable(msg.sender) {
-        token = _token;
-        nextDealId = 1;
-    }
+    constructor() Ownable(msg.sender) {}
 
-    function createDeal(address implementer, uint256 amount) external returns (uint256) {
-        uint256 dealId = nextDealId++;
-        deals[dealId] = Deal(msg.sender, implementer, amount, false, false);
+    function createDeal(uint256 id, address implementer, IERC20 token, uint256 amount) external returns (uint256) {
+        uint256 dealId = id;
+        deals[dealId] = Deal(msg.sender, implementer, token, amount, false, false);
         return dealId;
     }
 
     function depositTokens(uint256 dealId) external {
         Deal storage deal = deals[dealId];
         require(msg.sender == deal.client, "Only the client can deposit tokens for the deal.");
-        require(token.transferFrom(msg.sender, address(this), deal.amount), "Token transfer failed");
+        require(deal.token.transferFrom(msg.sender, address(this), deal.amount), "Token transfer failed");
         deal.fundsDeposited = true;
     }
 
     function completeDeal(uint256 dealId) external onlyOwner {
         Deal storage deal = deals[dealId];
         require(!deal.dealCompleted, "Deal is already completed.");
-        require(token.transfer(deal.implementer, deal.amount), "Token transfer failed");
+        require(deal.token.transfer(deal.implementer, deal.amount), "Token transfer failed");
         deal.dealCompleted = true;
     }
 
@@ -52,7 +48,7 @@ contract EscrowContract is Ownable {
         uint256 refundAmount = deal.amount;
         deal.fundsDeposited = false;
 
-        require(token.transfer(deal.client, refundAmount), "Refund failed");
+        require(deal.token.transfer(deal.client, refundAmount), "Refund failed");
     }
 
     function finalizeDeal(uint256 dealId, uint256 implementerPercentage) external onlyOwner {
@@ -64,9 +60,9 @@ contract EscrowContract is Ownable {
         uint256 implementerAmount = deal.amount * implementerPercentage / 100;
         uint256 clientRefund = deal.amount - implementerAmount;
 
-        require(token.transfer(deal.implementer, implementerAmount), "Transfer to implementer failed");
+        require(deal.token.transfer(deal.implementer, implementerAmount), "Transfer to implementer failed");
         if (clientRefund > 0) {
-            require(token.transfer(deal.client, clientRefund), "Refund to client failed");
+            require(deal.token.transfer(deal.client, clientRefund), "Refund to client failed");
         }
     }
 
